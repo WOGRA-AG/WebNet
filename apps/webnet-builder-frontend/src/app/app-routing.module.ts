b70634a1dd600b5c7dfbe211d63cb1b0e797f153
Andreas Müller <an.mueller91@gmail.com>
2023-08-18 20:51:10 +0200

    added gitignore and changed '/' to default route instead to 'home'

--- apps/webnet-builder-frontend/src/app/app-routing.module.ts
+++ apps/webnet-builder-frontend/src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import {HomeComponent} from "./pages/home/home.component";
 import {TestingComponent} from "./pages/testing/testing.component";
 
 const routes: Routes = [
-  {path: '', redirectTo:'home', pathMatch: 'full'},
-  {path: 'home',  component: HomeComponent, pathMatch: 'full'},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'home',  redirectTo: '', pathMatch: 'full'},
   {path: 'testing', component: TestingComponent, pathMatch:'full'},
-  {path: '**', redirectTo:'home'}
+  {path: '**', redirectTo:''}
 ];
 
 @NgModule({
