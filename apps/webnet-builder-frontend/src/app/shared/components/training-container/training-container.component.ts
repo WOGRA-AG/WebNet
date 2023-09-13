a39ea47ba4ba44864cbff11c5bf71906320296e5
Andreas Müller <an.mueller91@gmail.com>
2023-09-13 15:48:12 +0200

    removed more unused imports

--- apps/webnet-builder-frontend/src/app/shared/components/training-container/training-container.component.ts
+++ apps/webnet-builder-frontend/src/app/shared/components/training-container/training-container.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input} from '@angular/core';
-import {MnistDataService} from "../../../core/services/mnist-data.service";
 import '@tensorflow/tfjs-backend-webgpu';
 import '@tensorflow/tfjs-backend-wasm';
-import {ModelWrapperService} from "../../../core/services/model-wrapper.service";
 import {TrainingStats} from "../../../core/interfaces";
 
 @Component({
